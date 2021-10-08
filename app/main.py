829ac5d73ccde158720113e5b06ae2c1e42f6e90
Jason Washburn <wburn@wburn.net>
2021-10-07 20:51:15 -0500

    :tada: initial commit

---
+++ app/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+
+from app.db import database, User
+
+app = FastAPI(title="FastAPI, Docker, and Traefik")
+
+
+@app.get("/")
+async def read_root():
+    return await User.objects.all()
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+@app.on_event("startup")
+async def startup():
+    if not database.is_connected:
+        await database.connect()
+    # create a dummy entry
+    await User.objects.get_or_create(email="test@test.com")
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    if database.is_connected:
+        await database.disconnect()
